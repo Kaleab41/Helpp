a59b98fb5f5df15d55a754fea25da149589a920b
gokerod <abeelgirma@gmail.com>
2024-03-28 15:05:17 +0300

    Fixed DashboardTable

--- client/src/components/shared/dashboardTable/DashboardTable.tsx
+++ client/src/components/shared/dashboardTable/DashboardTable.tsx
@@ -1,53 +1,7 @@
-import { Button, Table } from "flowbite-react"
+import { Table } from "flowbite-react"
 import { ReactNode } from "react"
 
 interface DashboardProps {
-<<<<<<< HEAD
-    headers: string[];
-    tableData: Record<string, string>[];
-    includedData: string[];
-}
-
-const handleButtonClick = (row: {}) => {
-    console.log(row);
-}
-
-
-const DashboardTable = ({ headers, tableData, includedData }: DashboardProps) => {
-    return (
-        
-            <div className="overflow-x-auto">
-            <Table>
-              <Table.Head className="text-base uppercase">
-                {
-                    headers.map((header: string) => (
-                        <Table.HeadCell>{header}</Table.HeadCell>
-                    ))
-                }
-              </Table.Head>
-              <Table.Body className="divide-y"> 
-                {
-                    tableData.map((row, index) => (
-                        <Table.Row key={index} className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                            {
-                                Object.keys(row).map((key) => (
-                                    includedData.some(data => data === key) ? <Table.Cell key={key} className="text-sm odd:font-bold odd:text-black">{row[key]}</Table.Cell> : null
-                                ))
-                            }
-                            <Table.Cell key={`button-${index}`}>
-                                <Button onClick={() => handleButtonClick(row)}>
-                                    Request Change
-                                </Button>
-                            </Table.Cell>
-                        </Table.Row>
-                    ))
-                }
-              </Table.Body>
-            </Table>
-          </div>
-        
-    );
-=======
   headers: string[]
   tableData: Record<string, string>[]
   children?: ReactNode
@@ -75,7 +29,6 @@ const DashboardTable = ({ headers, tableData, children }: DashboardProps) => {
       </Table>
     </div>
   )
->>>>>>> c612169 (Added custom custom action button as children)
 }
 
 export default DashboardTable
