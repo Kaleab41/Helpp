7cc8e854d8fa322a85b6e37d7d1b425d0b8cf345
gokerod <abeelgirma@gmail.com>
2024-03-28 09:09:22 +0300

    Refactor student registration

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,13 +1,8 @@
-import { Button, Modal } from "flowbite-react"
+import { Modal } from "flowbite-react"
 import { useState } from "react"
-import { IRegistrationStudent } from "../../api/types/student.type.ts"
-import Input from "../form/Input.tsx"
-import Select from "../form/Select.tsx"
-import Textarea from "../form/Textarea.tsx"
-import FileInput from "../form/FileInput.tsx"
-import { useCreateStudentMutation } from "../../api/studentApi/student.slice.ts"
 import TeacherRegister from "./Teacher/TeacherRegister.tsx"
 import RoleMenu from "../form/RoleMenu.tsx"
+import StudentRegister from "./Student/StudentRegister.tsx"
 
 type SingupProp = {
   openRegisterModal: boolean
@@ -15,55 +10,11 @@ type SingupProp = {
 }
 
 export default function Register({ openRegisterModal, SetRegisterModal }: SingupProp) {
-  const [name, SetName] = useState<IRegistrationStudent["name"]>("")
-  const [gender, SetGender] = useState<IRegistrationStudent["gender"]>("")
-  const [email, SetEmail] = useState<IRegistrationStudent["email"]>("")
-  const [phone, SetPhone] = useState<IRegistrationStudent["phone"]>("")
-  const [guardianName, SetGuardianName] = useState<IRegistrationStudent["guardianName"]>("")
-  const [guardianPhone, SetGuardianPhone] = useState<IRegistrationStudent["guardianPhone"]>("")
-  const [aboutYou, SetAboutYou] = useState<IRegistrationStudent["aboutYou"]>("")
-  const [department, SetDepartment] = useState<IRegistrationStudent["department"]>("")
-  const [academicRecord, SetAcademicRecord] = useState<IRegistrationStudent["academicRecord"]>(null)
-
   const [role, SetRole] = useState<string>("Student")
 
-  const [create, {}] = useCreateStudentMutation()
   function onCloseModal() {
     SetRegisterModal(false)
-    SetName("")
-    SetGender("")
-    SetEmail("")
-    SetPhone("")
-    SetGuardianName("")
-    SetGuardianPhone("")
-    SetAboutYou("")
-    SetDepartment("")
-    SetAcademicRecord(null)
     SetRole("Student") //default role
-  }
-
-  const handleRegister = async () => {
-    try {
-      const formData = new FormData()
-      formData.append("name", name)
-      formData.append("gender", gender)
-      formData.append("email", email)
-      formData.append("phone", phone)
-      formData.append("guardianName", guardianName)
-      formData.append("guardianPhone", guardianPhone)
-      formData.append("aboutYou", aboutYou)
-      formData.append("department", department)
-      if (academicRecord) {
-        formData.append("academicRecord", academicRecord)
-      }
-      const response = await create(formData).unwrap()
-      if (response) {
-        onCloseModal()
-      }
-    } catch (error) {
-      const _error = (error as any).error
-      console.log({ _error })
-    }
   }
 
   return (
@@ -71,84 +22,7 @@ export default function Register({ openRegisterModal, SetRegisterModal }: Singup
       <Modal show={openRegisterModal} size="xl" onClose={onCloseModal} popup>
         <Modal.Header />
         <Modal.Body>
-          {role === "Student" && (
-            <div className="space-y-6">
-              {/* Form Title */}
-              <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                Apply to the school
-              </h3>
-
-              {/* Form Component */}
-              <div className="grid grid-cols-2 gap-2">
-                <Input
-                  name={"Name"}
-                  placeholder="Enter your name"
-                  setValue={SetName}
-                  type="text"
-                  value={name}
-                />
-
-                <Select name="Gender" options={["", "Male", "Female"]} setValue={SetGender} />
-              </div>
-              <div className="grid grid-cols-2 gap-2">
-                <Input
-                  name={"Email"}
-                  placeholder="Enter your email"
-                  setValue={SetEmail}
-                  type="text"
-                  value={email}
-                />
-                <Input
-                  name={"Phone"}
-                  placeholder="Enter your phone"
-                  setValue={SetPhone}
-                  type="text"
-                  value={phone}
-                />
-              </div>
-              <div className="grid grid-cols-2 gap-2">
-                <Input
-                  name={"Gurdian Name"}
-                  placeholder="Enter your gurdian name"
-                  setValue={SetGuardianName}
-                  type="text"
-                  value={guardianName}
-                />
-                <Input
-                  name={"Gurdian Phone"}
-                  placeholder="Enter your gurdian phone"
-                  setValue={SetGuardianPhone}
-                  type="text"
-                  value={guardianPhone}
-                />
-              </div>
-              <Input
-                name={"Department"}
-                placeholder="Enter your department"
-                setValue={SetDepartment}
-                type="text"
-                value={department}
-              />
-              <Textarea
-                name={"About you"}
-                placeholder="Enter description about you"
-                SetValue={SetAboutYou}
-                value={aboutYou}
-              />
-              <FileInput
-                name="Acadamic Record"
-                SetValue={SetAcademicRecord}
-                helperText="Upload your acadamic recored "
-              />
-              {/* Form Action */}
-              <div className="flex justify-center">
-                <Button type="submit" onClick={handleRegister}>
-                  Register
-                </Button>
-              </div>
-            </div>
-          )}
-
+          {role === "Student" && <StudentRegister />}
           {role === "Teacher" && <TeacherRegister />}
           <RoleMenu
             value={role}
--- client/src/components/auth/Student/StudentRegister.tsx
+++ client/src/components/auth/Student/StudentRegister.tsx
@@ -1,3 +1,132 @@
+import { Button } from "flowbite-react"
+import { IRegistrationStudent } from "../../../api/types/student.type"
+import { useState } from "react"
+import { useCreateStudentMutation } from "../../../api/studentApi/student.slice.ts"
+import Input from "../../form/Input"
+import Select from "../../form/Select.tsx"
+import FileInput from "../../form/FileInput.tsx"
+import Textarea from "../../form/FileInput.tsx"
+
 export default function StudentRegister() {
-  return <div>StudentRegister</div>
+  const [name, SetName] = useState<IRegistrationStudent["name"]>("")
+  const [gender, SetGender] = useState<IRegistrationStudent["gender"]>("")
+  const [email, SetEmail] = useState<IRegistrationStudent["email"]>("")
+  const [phone, SetPhone] = useState<IRegistrationStudent["phone"]>("")
+  const [guardianName, SetGuardianName] = useState<IRegistrationStudent["guardianName"]>("")
+  const [guardianPhone, SetGuardianPhone] = useState<IRegistrationStudent["guardianPhone"]>("")
+  const [aboutYou, SetAboutYou] = useState<IRegistrationStudent["aboutYou"]>("")
+  const [department, SetDepartment] = useState<IRegistrationStudent["department"]>("")
+  const [academicRecord, SetAcademicRecord] = useState<IRegistrationStudent["academicRecord"]>(null)
+
+  const [create, {}] = useCreateStudentMutation()
+  const handleRegister = async () => {
+    try {
+      const formData = new FormData()
+      formData.append("name", name)
+      formData.append("gender", gender)
+      formData.append("email", email)
+      formData.append("phone", phone)
+      formData.append("guardianName", guardianName)
+      formData.append("guardianPhone", guardianPhone)
+      formData.append("aboutYou", aboutYou)
+      formData.append("department", department)
+      if (academicRecord) {
+        formData.append("academicRecord", academicRecord)
+      }
+      const response = await create(formData).unwrap()
+      if (response) {
+        onCloseModal()
+      }
+    } catch (error) {
+      const _error = (error as any).error
+      console.error({ _error })
+    }
+  }
+
+  function onCloseModal() {
+    SetName("")
+    SetGender("")
+    SetEmail("")
+    SetPhone("")
+    SetGuardianName("")
+    SetGuardianPhone("")
+    SetAboutYou("")
+    SetDepartment("")
+    SetAcademicRecord(null)
+  }
+  return (
+    <div className="space-y-6">
+      {/* Form Title */}
+      <h3 className="text-xl font-medium text-gray-900 dark:text-white">Apply to the school</h3>
+
+      {/* Form Component */}
+      <div className="grid grid-cols-2 gap-2">
+        <Input
+          name={"Name"}
+          placeholder="Enter your name"
+          setValue={SetName}
+          type="text"
+          value={name}
+        />
+
+        <Select name="Gender" options={["", "Male", "Female"]} setValue={SetGender} />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <Input
+          name={"Email"}
+          placeholder="Enter your email"
+          setValue={SetEmail}
+          type="text"
+          value={email}
+        />
+        <Input
+          name={"Phone"}
+          placeholder="Enter your phone"
+          setValue={SetPhone}
+          type="text"
+          value={phone}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <Input
+          name={"Gurdian Name"}
+          placeholder="Enter your gurdian name"
+          setValue={SetGuardianName}
+          type="text"
+          value={guardianName}
+        />
+        <Input
+          name={"Gurdian Phone"}
+          placeholder="Enter your gurdian phone"
+          setValue={SetGuardianPhone}
+          type="text"
+          value={guardianPhone}
+        />
+      </div>
+      <Input
+        name={"Department"}
+        placeholder="Enter your department"
+        setValue={SetDepartment}
+        type="text"
+        value={department}
+      />
+      <Textarea
+        name={"About you"}
+        placeholder="Enter description about you"
+        SetValue={SetAboutYou}
+        value={aboutYou}
+      />
+      <FileInput
+        name="Acadamic Record"
+        SetValue={SetAcademicRecord}
+        helperText="Upload your acadamic recored "
+      />
+      {/* Form Action */}
+      <div className="flex justify-center">
+        <Button type="submit" onClick={handleRegister}>
+          Register
+        </Button>
+      </div>
+    </div>
+  )
 }
